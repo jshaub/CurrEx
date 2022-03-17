a26ad9c947f68c283f62555c337d190e33251d2f
Jon Shaub <shauby19@gmail.com>
2022-03-17 19:15:02 -0400

    Updated implementation of currency list
    - Modified implementation of supported currencies to be a dictionary (previously an array)
    - Implemented CurrencyList class to obscure direct use of currencies dictionary and promote use of class methods
    - Added logic checks on user input to enforce correct type of input

--- currencies.py
+++ currencies.py
@@ -1,42 +1,57 @@
 # Data and functions related to the currencies supported by CurrEx
 
-# Supported currencies (Code, Name)
-currencies = [
-	['EUR', 'Euro Member Countries'],
-	['IDR', 'Indonesia Rupiah'],
-	['BGN', 'Bulgaria Lev'],
-	['ILS', 'Israel Shekel'],
-	['GBP', 'United Kingdom Pound'],
-	['DKK', 'Denmark Krone'],
-	['CAD', 'Canada Dollar'],
-	['JPY', 'Japan Yen'],
-	['HUF', 'Hungary Forint'],
-	['RON', 'Romania New Leu'],
-	['MYR', 'Malaysia Ringgit'],
-	['SEK', 'Sweden Krona'],
-	['SGD', 'Singapore Dollar'],
-	['HKD', 'Hong Kong Dollar'],
-	['AUD', 'Australia Dollar'],
-	['CHF', 'Switzerland Franc'],
-	['KRW', 'Korea (South) Won'],
-	['CNY', 'China Yuan Renminbi'],
-	['TRY', 'Turkey Lira'],
-	['HRK', 'Croatia Kuna'],
-	['NZD', 'New Zealand Dollar'],
-	['THB', 'Thailand Baht'],
-	['USD', 'United States Dollar'],
-	['NOK', 'Norway Krone'],
-	['RUB', 'Russia Ruble'],
-	['INR', 'India Rupee'],
-	['MXN', 'Mexico Peso'],
-	['CZK', 'Czech Republic Koruna'],
-	['BRL', 'Brazil Real'],
-	['PLN', 'Poland Zloty'],
-	['PHP', 'Philippines Peso'],
-	['ZAR', 'South Africa Rand']
-];
+class CurrencyList:
 
-# Prints the list of supported currencies
-def printCurrencies():
-	for rows in currencies:
-		print("%s (%s)" %(rows[1], rows[0]));
+	# Dictionary of support currencies
+	# Formatted as: (Code, Complete Name)
+	currencies = {
+		"EUR": "Euro Member Countries",
+		"IDR": "Indonesia Rupiah",
+		"BGN": "Bulgaria Lev",
+		"ILS": "Israel Shekel",
+		"GBP": "United Kingdom Pound",
+		"DKK": "Denmark Krone",
+		"CAD": "Canada Dollar",
+		"JPY": "Japan Yen",
+		"HUF": "Hungary Forint",
+		"RON": "Romania New Leu",
+		"MYR": "Malaysia Ringgit",
+		"SEK": "Sweden Krona",
+		"SGD": "Singapore Dollar",
+		"HKD": "Hong Kong Dollar",
+		"AUD": "Australia Dollar",
+		"CHF": "Switzerland Franc",
+		"KRW": "Korea (South) Won",
+		"CNY": "China Yuan Renminbi",
+		"TRY": "Turkey Lira",
+		"HRK": "Croatia Kuna",
+		"NZD": "New Zealand Dollar",
+		"THB": "Thailand Baht",
+		"USD": "United States Dollar",
+		"NOK": "Norway Krone",
+		"RUB": "Russia Ruble",
+		"INR": "India Rupee",
+		"MXN": "Mexico Peso",
+		"CZK": "Czech Republic Koruna",
+		"BRL": "Brazil Real",
+		"PLN": "Poland Zloty",
+		"PHP": "Philippines Peso",
+		"ZAR": "South Africa Rand"
+	}
+
+	# Prints the list of supported currencies
+	def print(self):
+		for keys in self.currencies:
+			print("%s (%s)" %(keys, self.currencies.get(keys)));
+
+	# Return list of currencies
+	def getCurrencies(self):
+		return self.currencies;
+
+	# Check if given code exists in the currencies dictionary
+	# Returns true if it exists, and false otherwise
+	def checkCode(self, code):
+		if(self.currencies.get(code)):
+			return True;
+		else:
+			return False;
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 # Main runtime environment for CurrEx application
 from forex_python.converter import CurrencyRates
 from forex_python.converter import CurrencyCodes
-from currencies import *
+from currencies import CurrencyList
 
 # Get references to exchanges rates and currency symbols
 currencyRates = CurrencyRates();
 currencyCodes = CurrencyCodes();
+currList = CurrencyList();
+
+validCode = False; # Boolean used to determine if entered currency code is valid
 
 print("Welcome to CurrEx, the currency exchange program!");
 print("Please select the base currency from the following list:");
-printCurrencies();
+currList.print();
 
+# Get user input for base currency
 baseCurrency = input("Enter base currency code (ex. USD): ");
+
+while(not currList.checkCode(baseCurrency)):
+	baseCurrency = input("Invalid code selected. Please enter a new code: ");
+
+# Get user input for the target currenc
 exchangeCurrency = input("Enter the exchange currency code: ");
+
+while(not currList.checkCode(exchangeCurrency)):
+	exchangeCurrency = input("Invalid code selected. Please enter a new code: ");
+
+# Get user input for the amount of currency to convert
 currencyAmount = float(input("Enter currency amount: "));
 
 print("%.2f %s (%s) is %.2f %s (%s)" %(currencyAmount, baseCurrency, currencyCodes.get_symbol(baseCurrency),
